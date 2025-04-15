5b40f7dd9b4974dc078165f484bd7a97c0bd5a2c
Romanin <60302782+romanin-rf@users.noreply.github.com>
2025-04-16 01:55:08 +0200

    Formatting and fixing

--- setup.py
+++ setup.py
@@ -7,19 +7,11 @@ import os
 import os.path
 import sys
 from typing import TYPE_CHECKING, Any
-
-# we'll import stuff from the source tree, let's ensure is on the sys path
-sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
-
-# This requires setuptools when building; setuptools is not needed
-# when installing from a wheel file (though it is still needed for
-# alternative forms of installing, as suggested by README.md).
+from pathlib import Path
 from setuptools import Extension, find_packages, setup
+from mypyc.build import mypycify
 
-
-
-# from setuptools.command.build_py import build_py
-
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 
 if TYPE_CHECKING:
     from typing_extensions import TypeGuard
@@ -27,15 +19,7 @@ if TYPE_CHECKING:
 def is_list_of_setuptools_extension(items: list[Any]) -> TypeGuard[list[Extension]]:
     return all(isinstance(item, Extension) for item in items)
 
-
 def find_package_data(base, globs, root="rsv"):
-    """Find all interesting data files, for setup(package_data=)
-
-    Arguments:
-        root: The directory to search in.
-        globs: A list of glob patterns to accept files.
-    """
-
     rv_dirs = [root for root, dirs, files in os.walk(base)]
     rv = []
     for rv_dir in rv_dirs:
@@ -47,26 +31,21 @@ def find_package_data(base, globs, root="rsv"):
         rv.extend([os.path.relpath(f, root) for f in files])
     return rv
 
-
 package_data = ["py.typed"]
-
 package_data += find_package_data(os.path.join("rsv"), ["*.py", "*.pyi"])
-
 everything = [os.path.join("rsv", x) for x in find_package_data("rsv", ["*.py"])]
-
-from mypyc.build import mypycify
 
 opt_level = os.getenv("MYPYC_OPT_LEVEL", '3')
 debug_level = os.getenv("MYPYC_DEBUG_LEVEL", '1')
 force_multifile = os.getenv("MYPYC_MULTI_FILE", '') == '1'
+
 ext_modules = mypycify(
-    everything,  # + ["--config-file=mypy_bootstrap.ini"],
+    everything,
     opt_level=opt_level,
     debug_level=debug_level,
-    # Use multi-file compilation mode on windows because without it
-    # our Appveyor builds run out of memory sometimes.
     multi_file=sys.platform == "win32" or force_multifile,
 )
+
 assert is_list_of_setuptools_extension(ext_modules), "Expected mypycify to use setuptools"
 
 classifiers = [
@@ -84,11 +63,7 @@ classifiers = [
     "Typing :: Typed",
 ]
 
-from pathlib import Path
-
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
-
+long_description = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name="rsv",
@@ -99,7 +74,6 @@ setup(
     author="Romanin",
     author_email="semina054@gmail.com",
     keywords=["rsv", "csv", "io", "file", "format", "dump", "load", "read", "write"],
-
     url="https://github.com/romanin-rf/rsv",
     license="MIT",
     py_modules=[],
@@ -107,15 +81,10 @@ setup(
     packages=find_packages(),
     package_data={"rsv": package_data},
     entry_points={
-        "console_scripts": [
-            # "rsv=rsv.__main__:main",
-        ]
+        "console_scripts": []
     },
     classifiers=classifiers,
-    install_requires=[
-        # nothing!
-        "setuptools"
-    ],
+    install_requires=["setuptools"],
     python_requires=">=3.9",
     include_package_data=True,
     project_urls={
